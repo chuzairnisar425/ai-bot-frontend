0dd29a839976fc2d9e2de41dcc5b23ba5fe6f742
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2025-05-07 15:48:25 +0500

    modified

--- src/app/shared/components/ui/BroascastAlert.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IRootState } from '../../../store';
-// import Alert from '../../../features/Authentication/components/Alert';
-
-function BroascastAlert() {
-    return <></>;
-}
-
-export default BroascastAlert;
